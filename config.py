b539d05ba5dbcdfd8c98ca2363cd40e3f87283cb
kumagai <meteokuma@gmail.com>
2026-07-18 22:26:43 +0900

    added var

--- config.py
+++ config.py
@@ -7,14 +7,30 @@ PLOT_CONFIG = {
         "ctrl_grp": "fcst_sfc", #fcst_sfc anl_sfc, fcst_p, anl_p
         "grads_var": "APCPsfc",
         "gxout": "shaded",
-        "lat_range": (34, 38),
-        "lon_range": (135, 140),
+        "lat_range": (20, 50),
+        "lon_range": (120, 150),
     },
 
     #temperature
+    "temp": {
+        "label": "気温(2m)",
+        "ctrl_grp": "fcst_sfc", #fcst_sfc anl_sfc, fcst_p, anl_p
+        "grads_var": "TMP2m",
+        "gxout": "shaded",
+        "lat_range": (20, 50),
+        "lon_range": (120, 150),
+    },
 
     #wind
 
     #pressure
+    "prs": {
+        "label": "海面更正気圧",
+        "ctrl_grp": "fcst_sfc", #fcst_sfc anl_sfc, fcst_p, anl_p
+        "grads_var": "PRMSLmsl",
+        "gxout": "shaded",
+        "lat_range": (20, 50),
+        "lon_range": (120, 150),
+    },
 }
 
